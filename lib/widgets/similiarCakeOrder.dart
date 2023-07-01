1fae213a73562b7e95ba52975b10a7764a884249
vivekanand9229 <48760768+vivekanand9229@users.noreply.github.com>
2023-07-01 21:31:26 +0530

    code fixes

--- lib/widgets/similiarCakeOrder.dart
+++ lib/widgets/similiarCakeOrder.dart
@@ -10,40 +10,44 @@ import 'compressedImage.dart';
 
 class SimilarCakeOrder extends StatefulWidget {
   final String cakeChildCategoryId;
-  const SimilarCakeOrder({Key? key, required this.cakeChildCategoryId}) : super(key: key);
+
+  const SimilarCakeOrder({Key? key, required this.cakeChildCategoryId})
+      : super(key: key);
 
   @override
   State<SimilarCakeOrder> createState() => _SimilarCakeOrderState();
 }
 
 class _SimilarCakeOrderState extends State<SimilarCakeOrder> {
-
   bool loaded = false;
-  List randomCakes = [];
+  List<dynamic> randomCakes = [];
 
-  List _processCakes(Map cakeMap) {
-    List newItems = [];
-    for (var cake in cakeMap.entries) {
-      Map tempWeightPrice = {};
-      Map tempWeightPriceOffer = {};
-      for(var weights in cake.value["WeightPrice"]){
-        tempWeightPrice[double.parse(weights.split("<sep>")[0])] = double.parse(weights.split("<sep>")[1]);
+  List<Cake> _processCakes(Map<dynamic, dynamic> cakeMap) {
+    List<Cake> newItems = [];
+    cakeMap.entries.forEach((cake) {
+      Map<double, double> tempWeightPrice = {};
+      Map<double, double> tempWeightPriceOffer = {};
+      for (var weights in cake.value["WeightPrice"]) {
+        tempWeightPrice[double.parse(weights.split("<sep>")[0])] =
+            double.parse(weights.split("<sep>")[1]);
       }
       if (cake.value.containsKey("offerWeight")) {
-        for(var weights in cake.value["offerWeight"]){
-          tempWeightPriceOffer[double.parse(weights.split("<sep>")[0])] = double.parse(weights.split("<sep>")[1]);
+        for (var weights in cake.value["offerWeight"]) {
+          tempWeightPriceOffer[double.parse(weights.split("<sep>")[0])] =
+              double.parse(weights.split("<sep>")[1]);
         }
       } else {
         tempWeightPriceOffer = tempWeightPrice;
       }
       List<String> cakeImages = [];
-      if (cake.value["Image"].runtimeType.toString() == "String") {
-        cakeImages = ["https://res.cloudinary.com/maharani/image/upload/${cake.value["Image"].toString()}/image_asset/${cake.key}.jpg"];
+      if (cake.value["Image"] is String) {
+        cakeImages = [
+          "https://res.cloudinary.com/maharani/image/upload/${cake.value["Image"].toString()}/image_asset/${cake.key}.jpg"
+        ];
       } else {
         for (var images in cake.value["Image"]) {
           if (images != "") {
             cakeImages.add(images.toString());
-
           }
         }
       }
@@ -53,68 +57,58 @@ class _SimilarCakeOrderState extends State<SimilarCakeOrder> {
       bool isFavourite = wishList!.any((item) => item.contains(cake.key));
 
       newItems.add(
-          Cake(
-              firebaseId: cake.key,
-              imageUrl: cakeImages,
-              name: cake.value["Name"],
-              specs: cake.value["Specs"],
-              number: cake.value["No"],
-              weightPrice: tempWeightPrice,
-              weightPriceOffer: tempWeightPriceOffer,
-              offerAvailable: cake.value["offerApplicable"] ?? false,
-              inStock: cake.value["inStock"] ?? true,
-              description: cake.value["description"] ?? "", isFavourite: isFavourite
-          )
+        Cake(
+          firebaseId: cake.key,
+          imageUrl: cakeImages,
+          name: cake.value["Name"],
+          specs: cake.value["Specs"],
+          number: cake.value["No"],
+          weightPrice: tempWeightPrice,
+          weightPriceOffer: tempWeightPriceOffer,
+          offerAvailable: cake.value["offerApplicable"] ?? false,
+          inStock: cake.value["inStock"] ?? true,
+          description: cake.value["description"] ?? "",
+          isFavourite: isFavourite,
+        ),
       );
-    }
+    });
 
     return newItems;
   }
 
-
-  Future<void> _loadFromFirebase() async{
-
+  Future<void> _loadFromFirebase() async {
     setState(() {
       loaded = false;
     });
-    DatabaseEvent? event = await referenceGlobal
+
+    final event = await referenceGlobal
         .child("Categories")
         .child("CakesCategories")
         .child("ChildCategories")
         .child(widget.cakeChildCategoryId)
-        .child("Items")// limit to the first 20 items
-        .once(DatabaseEventType.value);
+        .child("Items")
+        .once();
 
-    Map? dataSnapshot = (event.snapshot.value ?? {}) as Map<dynamic, dynamic>;
+    final dataSnapshot = (event.snapshot.value ?? {}) as Map<dynamic, dynamic>;
+    final newItemsAll = _processCakes(dataSnapshot);
 
-    event = null;
-    List<dynamic>? newItemsAll = _processCakes(dataSnapshot);
-    dataSnapshot = null;
-    var rng = new Random();
-    var sampleSize = min(newItemsAll.length, 30);
-    var sample = List.from(newItemsAll);
-    newItemsAll = null;
+    final rng = Random();
+    final sampleSize = min(newItemsAll.length, 30);
+    final sample = List.from(newItemsAll);
 
     for (var i = 0; i < sampleSize; i++) {
-      var j = i + rng.nextInt(sample.length - i);
-      var temp = sample[i];
+      final j = i + rng.nextInt(sample.length - i);
+      final temp = sample[i];
       sample[i] = sample[j];
       sample[j] = temp;
     }
 
-    while(true) {
-      if (mounted) {
-        setState(() {
-          loaded = true;
-        });
-        break;
-      }
+    if (mounted) {
+      setState(() {
+        loaded = true;
+        randomCakes = sample.sublist(0, sampleSize);
+      });
     }
-
-
-    setState(() {
-      randomCakes = sample.sublist(0, sampleSize);
-    });
   }
 
   @override
@@ -125,65 +119,75 @@ class _SimilarCakeOrderState extends State<SimilarCakeOrder> {
 
   @override
   Widget build(BuildContext context) {
+    if (!loaded) {
+      return CircularProgressIndicator();
+    }
 
-    return loaded ? randomCakes.length > 0 ?
-    ListView.builder(
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        physics: ScrollPhysics(),
-        itemCount: randomCakes.length,
-        itemBuilder: (BuildContext context, int index){
-      return
-        GestureDetector(
-        onTap: (){
-          Navigator.pop(context);
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CakeItemScreen(cake: randomCakes[index], categoryId: 'CakesCategories', categoryMainId: widget.cakeChildCategoryId,)),);
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: [
-              Container(
-                width: 90.0,
-                height: 90.0,
-                decoration: BoxDecoration(
-                  // image: DecorationImage(
-                  //     image: OptimizedCacheImageProvider(
-                  //         elements.imageUrl[0]
-                  //     ),
-                  //     fit: BoxFit.cover
-                  // ),
-                    borderRadius: BorderRadius.circular(15.0)
+    if (randomCakes.isEmpty) {
+      return Text("No similar cakes found");
+    }
+
+    return
+      SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(randomCakes.length, (index) =>   GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CakeItemScreen(
+                  cake: randomCakes[index],
+                  categoryId: 'CakesCategories',
+                  categoryMainId: widget.cakeChildCategoryId,
                 ),
-                child: CompressedImage(imageUrl: randomCakes[index].imageUrl[0], quality: 5),
               ),
-              SizedBox(height: 10.0,),
-
-              Column(
-                children: [
-                  Text(
-                    "${randomCakes[index].name.split(" ")[0]}...",
-                    style: TextStyle(
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 90.0,
+                  height: 90.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: CompressedImage(
+                    imageUrl: randomCakes[index].imageUrl[0],
+                    quality: 5,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Column(
+                  children: [
+                    Text(
+                      "${randomCakes[index].name.split(" ")[0]}...",
+                      style: TextStyle(
                         color: Colors.black,
                         fontSize: 10,
                         fontWeight: FontWeight.bold,
-                        fontFamily: "Candarai"
+                        fontFamily: "Candarai",
+                      ),
                     ),
-                  ),
-                  Text(
-                    '\u{20B9} ${randomCakes[index].weightPrice[randomCakes[index].weightPrice.keys.toList()[0]]}',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
+                    Text(
+                      '\u{20B9} ${randomCakes[index].weightPrice[randomCakes[index].weightPrice.keys.toList()[0]]}',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      );
-    }) : Text("No similar cakes found") : CircularProgressIndicator();
+        )),
+      )
+     
+    );
   }
 }
